a1bc37ace480508db7b007710136dd0b3003cbc9
Mohamed Abdo <mohamadabdo@gmail.com>
2017-11-20 00:18:49 +0400

    Add timer for execution
    v1.1

--- decorator.py
+++ decorator.py
@@ -1,14 +1,17 @@
+from datetime import datetime
 def interceptor(before=None,after=None,onSuccess=None,onError=None):
     def decorator(func):
         def call(*args,**kwargs):
             try:
                 if before is not None:
                     before()
-                result=func(*args,**kwargs)
+                startTM = datetime.now()
+                result = func(*args,**kwargs)
+                timeEP = datetime.now() - startTM
                 if after is not None:
                     after()
                 if onSuccess is not None:
-                    onSuccess(result)
+                    onSuccess(result,';time ellipased =>', timeEP.total_seconds() * 1000)
                 return result
             except Exception as ex:
                 if onError is not None:
@@ -19,9 +22,12 @@ def interceptor(before=None,after=None,onSuccess=None,onError=None):
 @interceptor(
     before=lambda:print('Before executing ....'),
     after=lambda:print('After executing ....'),
-    onSuccess=lambda x: print('Result =>',x),
-    onError=lambda x:print(x))
+    onSuccess=lambda *x: print('Result =>',*x),
+    onError=lambda *x:print(*x))
 def SayHello():
     print("Hello world;")
     return 1010;
 SayHello()
+
+if __name__ == '__main__':
+    print('Hello main ....!')
