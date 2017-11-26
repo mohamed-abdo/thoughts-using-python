e22158dd708d492610e13265efaf3348f53a9e48
Mohamed Abdo <mohamadabdo@gmail.com>
2017-11-26 11:24:44 +0400

    add interceptor logger
    v1.2

--- map-reducer.py
+++ map-reducer.py
@@ -1,9 +1,23 @@
+def LoggerWrapper():
+    def deocrated(func):
+        def call(*args,**kwargs):
+            result=func(*args,**kwargs)
+            print('result logging => ',result)
+            return result
+        return call
+    return deocrated
+
 from functools import reduce
+
 def doubleNum(i):
     return i*2
+def BinaryMulitpNum(i):
+    return 2 ** i
 def evenNum(i):
     return i % 2 == 0
 ar=range(0,10)
+
+@LoggerWrapper()
 def mapReducre(xf,predicate,iterateble):
     def _reducer(accumlate,value):
         if predicate(value):
@@ -12,5 +26,8 @@ def mapReducre(xf,predicate,iterateble):
         else:
             return accumlate
     return reduce(_reducer,iterateble,[])
-result=mapReducre(doubleNum,evenNum,ar)
-print('map reducre of list:',list(ar),' is after double even numbers:',result)
+result=mapReducre(doubleNum, evenNum, ar)
+print('map reducre of list:',list(ar),' is after getting double of the even numbers:',result)
+
+result=mapReducre(BinaryMulitpNum, evenNum, ar)
+print('map reducre of list:',list(ar),' is after getting binay multiplication of the even numbers:',result)
